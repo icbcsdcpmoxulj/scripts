cb9c594c9d32f36af3b7547985330e35b35ee408
icbcsdcpmoxulj <icbcsdcpmoxulj@outlook.com>
2014-09-01 11:21:04 +0800

    add comment

--- maven/ezdeploy.bat
+++ maven/ezdeploy.bat
@@ -1,4 +1,20 @@
 @echo off
+Rem  Licensed to the Apache Software Foundation (ASF) under one or more
+Rem  contributor license agreements.  See the NOTICE file distributed with
+Rem  this work for additional information regarding copyright ownership.
+Rem  The ASF licenses this file to You under the Apache License, Version 2.0
+Rem  (the "License") you may not use this file except in compliance with
+Rem  the License.  You may obtain a copy of the License at
+Rem  
+Rem       http://www.apache.org/licenses/LICENSE-2.0
+Rem  
+Rem  Unless required by applicable law or agreed to in writing, software
+Rem  distributed under the License is distributed on an "AS IS" BASIS,
+Rem  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+Rem  See the License for the specific language governing permissions and
+Rem  limitations under the License.
+Rem  Contributors:      Xu Lijia
+
 echo Maven artifacts easy-deployer of 22nd August 2014, by Xu Lijia.
 echo.
 
